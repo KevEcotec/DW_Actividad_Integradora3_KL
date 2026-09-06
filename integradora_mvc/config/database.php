9a6df2aa76625623c469907ab39fda07553c3075
KevEcotec <k.lara@ecotec.edu.ec>
2026-09-06 07:50:49 -0500

    Correct typo in Database connection method
    Fix typo in setAttribute method call.

--- integradora_mvc/config/database.php
+++ integradora_mvc/config/database.php
@@ -1,1 +1,25 @@
+<?php
+class Database {
+  private $host = "localhost";
+  private $db_name = "Db_Integradora";
+  private $username = "root";
+  private $password = "";
+  public $conn;
 
+
+  public function getConnection() {
+    $this->conn = null;
+
+    try {
+      $this->conn = new PDO(
+        "mysql:host=" . $this->host . "dbname=" . $this->dbname,
+        $this->username,
+        $this->password
+      );
+      $this->conn->>setAttributu(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch(PDOException $exception) {
+      echo "Error de conexión:" . $exception->getMessage();
+    }
+    return $this->conn;
+  }
+}
