78990bd94ff6b53beffa3d7b4c415ce2e7ff1512
KevEcotec <k.lara@ecotec.edu.ec>
2026-09-07 21:47:35 -0500

    Refactor Cliente class for consistency and clarity

--- integradora_mvc/models/Cliente.php
+++ integradora_mvc/models/Cliente.php
@@ -1,46 +1,41 @@
 <?php
-require_once __DIR__ ."/../config/database.php";
+require_once __DIR__ . '/../config/database.php';
 
 class Cliente {
-  private $conn;
-  private $table_name = "Tbl_Clientes";
+    private $conn;
+    private $table_name = "Tbl_Clientes";
 
-  public $id;
-  public $nombre;
-  public $correo;
-  public $telefono;
-  public $edad;
+    public $id;
+    public $nombre;
+    public $correo;
+    public $telefono;
+    public $edad;
 
-  public function __construct() {
-    $database = new Database();
-    $this->conn = $database-> getConnection();
-  }
-
-  //Insertar registro
-  public function crear() {
-    $query = "INSERT INTO" . $this->table_name . " (clie_Nombre, clie_Correo, clie_Telefono, clie_Edad)
-              VALUES (:nombre, :correo, :telefono, :edad)";
-
-    $stmt = $this->conn->prepare($query);
-    
-    $stmt->bindParam(":nombre", $this->nombre);
-    $stmt->bindParam(":correo", $this->correo);
-    $stmt->bindParam(":nombre", $this->telefono);
-    $stmt->bindParam(":nombre", $this->edad);
-
-    if ($stmt->execute()){
-        return true;
+    public function __construct() {
+        $database = new Database();
+        $this->conn = $database->getConnection();
     }
 
-    return false;
-  }
+    public function crear() {
+        $query = "INSERT INTO " . $this->table_name . " 
+                 (clie_Nombre, clie_Correo, clie_Telefono, clie_Edad)
+                 VALUES (:nombre, :correo, :telefono, :edad)";
+        $stmt = $this->conn->prepare($query);
 
-  //Consultar todos los registros
-  public function obtenerTodos() {
-    $query = "SELECT * FROM" . $this->table_name . "ORDER BY clie_FechaRegistro DESC";
-    $stmt = this->conn->prepare($query);
-    $stmt->execute();
+        $stmt->bindParam(":nombre", $this->nombre);
+        $stmt->bindParam(":correo", $this->correo);
+        $stmt->bindParam(":telefono", $this->telefono);
+        $stmt->bindParam(":edad", $this->edad);
 
-    return $stmt;
-  }
+        return $stmt->execute();
+    }
+
+    public function obtenerTodos() {
+        $query = "SELECT * FROM " . $this->table_name . " ORDER BY clie_FechaRegistro DESC";
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute();
+        return $stmt;
+    }
 }
+?>
+
