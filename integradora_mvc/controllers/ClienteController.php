98ab6405a35c9d8c621cff99a764d86fbf997177
KevEcotec <k.lara@ecotec.edu.ec>
2026-09-07 21:46:39 -0500

    Refactor ClienteController to manage client views

--- integradora_mvc/controllers/ClienteController.php
+++ integradora_mvc/controllers/ClienteController.php
@@ -1,47 +1,27 @@
 <?php
-require_once __DIR__ . "/../config/database.php";
+require_once _DIR_ . '/../models/Cliente.php';
 
-class Cliente {
-    private $conn;
-    private $table_name = "Tbl_clientes";
-
-    public $id;
-    public $nombre;
-    public $correo;
-    public $telefono;
-    public $edad;
-
-    public function __construct() {
-        $database = new Database();
-        $this->conn = $database->getConnection();
+class ClienteController {
+    public function index(){
+        $cliente = new Cliente();
+        $resultados = $cliente->obtenerTodos();
+        include _DIR_ . '/../views/cliente/index.php';
     }
 
-    // Insertar registro
     public function crear() {
-        $query = "INSERT INTO " . $this->table_name . " (nombre, correo, telefono, edad)
-                  VALUES (:nombre, :correo, :telefono, :edad)";
-
-        $stmt = $this->conn->prepare($query);
-
-        $stmt->bindParam(":nombre", $this->nombre);
-        $stmt->bindParam(":correo", $this->correo);
-        $stmt->bindParam(":telefono", $this->telefono);
-        $stmt->bindParam(":edad", $this->edad);
-
-        if ($stmt->execute()) {
-            return true;
-        }
-
-        return false;
+        include _DIR_ . '/../views/cliente/crear.php';
     }
 
-    // Consultar todos los registros
-    public function obtenerTodos() {
-        $query = "SELECT * FROM " . $this->table_name . " ORDER BY fecha_registro DESC";
-        $stmt = $this->conn->prepare($query);
-        $stmt->execute();
+    public function guardar() {
+        $cliente = new Cliente();
+        $cliente->nombre = $_POST["nombre"];
+        $cliente->correo = $_POST["correo"];
+        $cliente->telefono = $_POST["telefono"];
+        $cliente->edad = $_POST["edad"];
+        $cliente->crear();
 
-        return $stmt;
+        header("Location: index.php?action=index");
+        exit;
     }
 }
-
+?>
