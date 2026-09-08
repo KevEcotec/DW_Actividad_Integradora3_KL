4c4773151a8817b8f9833c46991df67bcf3ac498
KevEcotec <k.lara@ecotec.edu.ec>
2026-09-07 21:34:46 -0500

    Fix database connection error handling

--- integradora_mvc/config/database.php
+++ integradora_mvc/config/database.php
@@ -6,20 +6,20 @@ class Database {
   private $password = "";
   public $conn;
 
-
   public function getConnection() {
     $this->conn = null;
-
-    try {
+    try{
       $this->conn = new PDO(
-        "mysql:host=" . $this->host . "dbname=" . $this->dbname,
+        "mysql:host=" . $this->host . ";dbname=" .$this->db_name . ";charset=utf8",
         $this->username,
         $this->password
       );
-      $this->conn->>setAttributu(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    } catch(PDOException $exception) {
-      echo "Error de conexión:" . $exception->getMessage();
+      $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $exception) {
+       echo "Error de conexión: " . $exception->getMessage(); 
     }
     return $this->conn;
   }
 }
+
+?>
